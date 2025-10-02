bc92944433f3dbfca1eb0986cfef65734951d795
Hessigawa <finelgahessigawa_2428250012@mhs.mdp.ac.id>
2025-10-02 11:11:03 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\TicketController;
 use Illuminate\Routing\Route;
 
 Route::get('event', [EventController::class, 'index']);
@@ -8,7 +9,7 @@ Route::post('event', [EventController::class, 'store']);
 Route::patch('event/{id}', [EventController::class, 'update']);
 Route::delete('event/{id}', [EventController::class, 'destroy']);
 
-Route::get('ticket', [EventController::class, 'index']);
-Route::post('ticket', [EventController::class, 'store']);
-Route::patch('ticket/{id}', [EventController::class, 'update']);
-Route::delete('ticket/{id}', [EventController::class, 'destroy']);
+Route::get('ticket', [TicketController::class, 'index']);
+Route::post('ticket', [TicketController::class, 'store']);
+Route::patch('ticket/{id}', [TicketController::class, 'update']);
+Route::delete('ticket/{id}', [TicketController::class, 'destroy']);
